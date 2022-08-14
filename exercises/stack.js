c153b6d0ab97359bfa8f83499c66557cfbef7dd9
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-08-14 11:43:12 -0300

    solve stack problem implementation

---
+++ exercises/stack.js
@@ -0,0 +1,24 @@
+const _items = new WeakMap();
+
+class Stack {
+    constructor() {
+        _items.set(this, []);
+    }
+    push(item) {
+        const items = _items.get(this);
+        items.push(item);
+    }
+    peek() {
+        const items = _items.get(this);
+        if (items.length === 0) throw new Error('Stack is empty');
+        return items[items.length - 1];
+    }
+    pop() {
+        const items = _items.get(this);
+        if (items.length === 0) throw new Error('Stack is empty');
+        return _items.get(this).pop();
+    }
+    get count() {
+        return _items.get(this).length;
+    }
+}
