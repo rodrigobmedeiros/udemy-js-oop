2185cfae13ac3eb374e8b72a50ed2e7c26f629ef
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-08-14 09:23:21 -0300

    finish section for private members using weakmaps

---
+++ private_members_weakmaps.js
@@ -0,0 +1,18 @@
+const _radius = new WeakMap();
+const _move = new WeakMap();
+
+class Circle {
+    constructor(radius) {
+        _radius.set(this, radius);
+        _move.set(this, () => {
+            console.log('move', this);
+        })
+    }
+    draw() {
+        _move.get(this)();
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(1);
+circle.draw();
